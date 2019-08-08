051827707af77c1735c5b853bacb9a6ccb0a5eae
SJH <1039460820@qq.com>
2019-08-08 19:49:58 +0800

    add datatypr

---
+++ src/datatype.ts
@@ -0,0 +1,12 @@
+//原始类型
+let bool: boolean = true;
+let num:number = 123;
+let str: string = 'abc'
+
+
+//数组
+let arr1: number[] = [1,2,3]
+let arr2: Array<number> = [1,2,3]
+
+//元祖
+let tuple = [number, string] = [0, '1']
